88d364c81b220ae0cb754ac71a3bc0468162af43
Juandi004 <jdgutierrez.2@sudamericano.edu.ec>
2024-06-11 15:21:29 -0500

    fixed

--- src/ball/dto/create-ball.dto.ts
+++ src/ball/dto/create-ball.dto.ts
@@ -1,19 +1,24 @@
-// create-ball.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateBallDto {
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly type: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   readonly price: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   readonly stock: number;
